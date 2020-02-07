4802a4b9b7959cb0769f5cc58e48ad2e782faee3
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-02-07 23:02:07 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,17 +1,14 @@
-school = School.new("Bayside High School")
-
-
-hash["new_key"] = []
-hash["new_key"] << "new_value_for_value_array"
- 
-hash
- => {"new_key"=>["new_value_for_value_array"]} 
-  end 
+class School
+  attr_accessor
+  attr_reader :name
   
-school.add_student("Zach Morris", 9)
-school.roster
-
-school.add_student("AC Slater", 9)
-school.add_student("Kelly Kapowski", 10)
-school.add_student("Screech", 11)
-end
+  ROSTER:
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  
+  
+  
+  
